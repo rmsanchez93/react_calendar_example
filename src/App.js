733e4569f82df56c01d6737ead10be4927564cda
Raul Sanchez <raulsg93@gmail.com>
2020-07-26 12:49:15 -0500

    playing with google-calendar-api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ApiCalendar from 'react-google-calendar-api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component {
+  state={
+    signedIn: false //track of auth token
+  }
+
+  handleClick=(target)=>{
+
+    if(target.name == 'Sign In'){
+      console.log('signing into google');
+      ApiCalendar.handleAuthClick();
+      this.setState({
+        signedIn:true
+      })
+    }
+    else if(target.name == 'Sign Out'){
+      console.log('signing outta google');
+      ApiCalendar.handleSignoutClick();
+    }
+    else{
+      alert('wtf this shouldnt run, something wrong')
+    }
+
+  }
+
+  getEvents = () => {
+    console.log('get events')
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.state.signedIn ? <button onClick={this.getEvents}>Click to see calendar in console!</button><img src={logo} className="App-logo" alt="logo" />}
+          <p>
+          <code>testing google calendar api</code>
+          </p>
+          <button name= "Sign In" onClick = {(e)=> this.handleClick(e.target)}>Click to sign in to Google</button>
+          <button name= "Sign Out" onClick = {(e)=> this.handleClick(e.target)}>Click to sign out of Google</button>
+        </header>
+      </div>
+    );
+
+  }
 }
-
-export default App;
